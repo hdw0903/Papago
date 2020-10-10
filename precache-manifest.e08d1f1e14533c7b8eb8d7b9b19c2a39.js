d3e7b248c7a8493bc1e42a4f359415858ce97d9b
Han Dongwon <hdw0903@naver.com>
2020-10-10 18:48:48 +0900

    Updates

---
+++ precache-manifest.e08d1f1e14533c7b8eb8d7b9b19c2a39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbec47010400d8a32231673329946edb",
+    "url": "/papago/index.html"
+  },
+  {
+    "revision": "f4387af738820d416709",
+    "url": "/papago/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "82202fac11fce0f54f79",
+    "url": "/papago/static/js/2.4404bbfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/papago/static/js/2.4404bbfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4387af738820d416709",
+    "url": "/papago/static/js/main.5af79b61.chunk.js"
+  },
+  {
+    "revision": "7d89fd9a8090921cf22e",
+    "url": "/papago/static/js/runtime-main.44283726.js"
+  }
+]);
