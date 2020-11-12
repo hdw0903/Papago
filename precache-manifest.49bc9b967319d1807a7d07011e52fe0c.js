52aec8b893f168910e39d90be2ec28815794ac0d
Han Dongwon <hdw0903@naver.com>
2020-11-12 16:49:39 +0900

    Updates

--- precache-manifest.49bc9b967319d1807a7d07011e52fe0c.js
+++ precache-manifest.49bc9b967319d1807a7d07011e52fe0c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0cae9a62aca81077273584f5b3c8403d",
+    "revision": "65323f1acd7c0b5cbe4cb8ac4b7f73e1",
     "url": "/papago/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/papago/static/css/2.d34346ea.chunk.css"
   },
   {
-    "revision": "7549e3907a309ef834a7",
+    "revision": "aabce270c1aa76cfc54f",
     "url": "/papago/static/css/main.95e76b59.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/papago/static/js/2.16e4f3e3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7549e3907a309ef834a7",
-    "url": "/papago/static/js/main.95005b91.chunk.js"
+    "revision": "aabce270c1aa76cfc54f",
+    "url": "/papago/static/js/main.7fe392a9.chunk.js"
   },
   {
     "revision": "7d89fd9a8090921cf22e",
