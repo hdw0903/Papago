dac08c5989f859737101f37d7844eca8780331ea
Han Dongwon <hdw0903@naver.com>
2020-11-26 20:52:03 +0900

    dropdown-outSide-click

--- src/components/DropdownSelectBox.js
+++ src/components/DropdownSelectBox.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useMemo } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
 const DropdownSelectBox = ({
   text,
@@ -8,18 +7,43 @@ const DropdownSelectBox = ({
   li,
   p,
 }) => {
+  const [dropdownState, setDropdownState] = useState(false);
   const optionalClassName = useMemo(
     () => `${isResponsive && 'responsive'} ${isDefault && 'default'}`,
     [isResponsive, isDefault]
   );
-
+  const spanClick = () => {
+    setDropdownState(!dropdownState);
+  };
+  const bodyClick = (e) => {
+    const target = e.target.className;
+    const ExclusionTarget =
+      target.includes('dropdown_text') || target.includes('selectedLang');
+    if (!ExclusionTarget) {
+      setDropdownState(false);
+    }
+  };
+  useEffect(() => {
+    document.body.addEventListener('click', bodyClick);
+    return () => {
+      document.body.removeEventListener('click', bodyClick);
+    };
+  }, []);
   return (
     <div className={`dropdown_lang ${optionalClassName}`}>
-      <span className={`dropdown_text ${optionalClassName}`}>
+      <span
+        className={`dropdown_text ${optionalClassName}`}
+        onClick={spanClick}
+      >
         {text}
-        <p>{p}</p>
+        <p className="selectedLang">{p}</p>
       </span>
-      <ul className={`dropdown_lang_select ${optionalClassName}`}>{li}</ul>
+      <ul
+        className={`dropdown_lang_select ${optionalClassName} 
+          ${dropdownState && 'dropdown_clicked'}`}
+      >
+        {li}
+      </ul>
     </div>
   );
 };
