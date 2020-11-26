724bc906451038cfdeb95bb353353cec26c886f4
Han Dongwon <hdw0903@naver.com>
2020-11-26 20:50:24 +0900

    주석 제거

--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -10,13 +10,6 @@ const TranslateContainer = ({
   readOnly = false,
   type,
 }) => {
-  //   const onChangeInput = (e) => {
-  //     console.log(e);
-  //     setInputValue(e.target.value);
-  //   };
-  //   const search = () => {2
-  //     clearDebounce();
-  //   };
   return (
     <textarea
       className={className}
@@ -28,16 +21,6 @@ const TranslateContainer = ({
       autoFocus={autoFocus}
       readOnly={readOnly}
     />
-    /* <div className="menu_button">
-        <CopyButton text={inputValue} toastNotify={toastNotify} />
-        <button onClick={search}>
-          <img
-            className="button_img"
-            src={process.env.PUBLIC_URL + '/img/enter_icon.png'}
-            alt="번역 버튼"
-          />
-        </button>
-      </div> */
   );
 };
 
