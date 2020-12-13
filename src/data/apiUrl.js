10c1b6ddaa97d34c0b8cab87d13a6f6890cba106
Han Dongwon <hdw0903@naver.com>
2020-12-13 17:35:11 +0900

    env.secrets

--- src/data/apiUrl.js
+++ src/data/apiUrl.js
@@ -1,4 +1,4 @@
-const endpoint = process.env.API_URL || 'http://localhost:5000';
+const endpoint = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 const url = {
   detectURL: `${endpoint}/api/papago/detectionLangs`,
   translateURL: `${endpoint}/api/papago/translation`,
