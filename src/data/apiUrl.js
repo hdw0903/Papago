523760c4af04adf116277ad89c4b7cfe78ba40b9
Han Dongwon <hdw0903@naver.com>
2020-12-13 17:48:17 +0900

    aws-texst

--- src/data/apiUrl.js
+++ src/data/apiUrl.js
@@ -1,4 +1,6 @@
-const endpoint = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const endpoint =
+  process.env.REACT_APP_API_URL ||
+  'http://mynodeexpress-env.eba-439z5hhp.ap-northeast-2.elasticbeanstalk.com';
 const url = {
   detectURL: `${endpoint}/api/papago/detectionLangs`,
   translateURL: `${endpoint}/api/papago/translation`,
